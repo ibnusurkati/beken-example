5596f6c00481a9a02a10702fcdde7423993701c5
Ibnu Surkati <dev.surkati@gmail.com>
2022-06-14 14:55:30 +0800

    fix : add event native

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator, NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Button, StatusBar, StyleSheet, Text } from 'react-native'
+import { Button, Share, StatusBar, StyleSheet, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { WebView, WebViewNavigation } from 'react-native-webview'
+import { WebView, WebViewMessageEvent, WebViewNavigation } from 'react-native-webview'
+
+type PayloadEvent = {
+  event: 'download' | 'share' | 'print'
+  productCode: string
+  data: string
+}
 
 const Stack = createNativeStackNavigator()
 
@@ -46,11 +52,60 @@ const BekenScreen = () => {
   urlPwa += `publickey=${encodeURIComponent('public-key')}&`
   urlPwa += `secretkey=${encodeURIComponent('secret-key')}`
 
+  const onDownload = async (productCode: string, data: string) => {
+    try {
+      await Share.share({
+        message: data,
+        title: 'Share Struk - ' + productCode
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onShare = async (productCode: string, data: string) => {
+    try {
+      await Share.share({
+        message: data,
+        title: 'Share Struk - ' + productCode
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onPrint = async (productCode: string, data: string) => {
+    try {
+      await Share.share({
+        message: data,
+        title: 'Share Struk - ' + productCode
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const onMessage = (event: WebViewMessageEvent) => {
+    const payload: PayloadEvent = JSON.parse(event.nativeEvent.data)
+    switch (payload.event) {
+      case 'download':
+        // your code in here
+        break;
+      case 'share':
+        // your code in here
+        break;
+      case 'print':
+        // your code in here
+        break;
+    }
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#1E1E1E" />
       <WebView
         source={{ uri: urlPwa }}
+        onMessage={onMessage}
         javaScriptEnabled={true}
         domStorageEnabled={true}
       />
